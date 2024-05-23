24f30a9005a96b781d2df44d0b443419943c626a
Yesaya Jones <yesayajones@Yesayas-Air.lan>
2024-05-23 14:46:45 -0500

    feat: Add Todo.js and TodWrapper

---
+++ todo-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWraper } from './components/TodoWraper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWraper/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list/src/components/TodoWraper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TodoForm } from './TodoForm'
+
+export const TodoWraper = () => {
+  return (
+    <div className='TodoWrapper'>
+        <TodoForm />
+    </div>
+  )
+}
